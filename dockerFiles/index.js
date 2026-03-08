9206b5d19fd0264c8f62a8d9d55e55cd80bce4a6
vigneswaran <vigneswaranoff@gmail.com>
2026-03-09 02:40:35 +0530

    built application

--- dockerFiles/index.js
+++ dockerFiles/index.js
@@ -1,1 +1,9 @@
-console.log("Welcome!!")
+const app = require("express")()
+
+app.listen(3000,()=>{
+    console.log("app is listening...")
+})
+
+app.get("/",(req,res)=>{
+    res.send("You are connected")
+})
