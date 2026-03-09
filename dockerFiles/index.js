d36ef88ad52da662fe61cc55655b5bf96cf72d9e
vigneswaran <vigneswaranoff@gmail.com>
2026-03-09 17:26:03 +0530

    modified index.js

--- dockerFiles/index.js
+++ dockerFiles/index.js
@@ -1,21 +1,26 @@
 const app = require("express")()
 const fs = require("fs")
 const path = require("path")
+const axios = require("axios")
+
+
 const server = app.listen(3000,()=>{
     console.log("app is listening...")
+    client = axios.get("http://localhost:3000/health")
 })
 
-app.use((req,res,next)=>{
+app.use("/health",(req,res,next)=>{
     file = fs.writeFile("log.txt","request from "+req.socket.remoteAddress,(err)=>{
         if(err) throw err
     })
-    next()
+    next("route")
 })
 app.get("/",(req,res)=>{
     res.send("You are connected")
 })
 
 app.get("/health",(req,res)=>{
+    console.log("Logged in")
     if(process.argv.includes("--once")){
         res.send("App is up and running")
         server.close(()=>{process.exit(0)})
