728b5cbd586c167383474377adcb7365d394ea65
vigneswaran <vigneswaranoff@gmail.com>
2026-03-09 16:41:28 +0530

    modified package.json

--- dockerFiles/index.js
+++ dockerFiles/index.js
@@ -1,9 +1,24 @@
 const app = require("express")()
-
-app.listen(3000,()=>{
+const fs = require("fs")
+const path = require("path")
+const server = app.listen(3000,()=>{
     console.log("app is listening...")
 })
 
+app.use((req,res,next)=>{
+    console.log(req)
+    file = fs.writeFile("log.txt","request from "+req.socket.remoteAddress,(err)=>{
+        if(err) throw err
+    })
+    next()
+})
 app.get("/",(req,res)=>{
     res.send("You are connected")
+})
+
+app.get("/health",(req,res)=>{
+    if(process.argv.includes("--once")){
+        res.send("App is up and running")
+        server.close(()=>{process.exit(0)})
+    }
 })
